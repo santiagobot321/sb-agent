76f208033dba1c0de869b4d54c1f93998b94442b
santiagobot321 <boteroramirezsantiago@gmail.com>
2025-12-01 10:25:32 -0500

    setting up wol

--- agent/sysadmin/executor.py
+++ agent/sysadmin/executor.py
@@ -1,5 +1,4 @@
 # agent/sysadmin/executor.py
-
 import os
 import subprocess
 
@@ -19,7 +18,22 @@ def update_system():
 # ----------- MULTIMEDIA --------------------
 
 def show_video(video_path):
-    subprocess.Popen(["xdg-open", video_path])
+
+    env = os.environ.copy()
+    env["DISPLAY"] = ":0"
+
+    # Detect XAUTHORITY for GNOME/GDM
+    if "XAUTHORITY" not in env or not os.path.exists(env.get("XAUTHORITY", "")):
+        posible_xauth = f"/run/user/{os.getuid()}/gdm/Xauthority"
+        if os.path.exists(posible_xauth):
+            env["XAUTHORITY"] = posible_xauth
+
+    try:
+        subprocess.Popen(["xdg-open", video_path], env=env)
+        print(f"[INFO] Video opened: {video_path}")
+    except Exception as e:
+        print(f"[ERROR] Could not open video: {e}")
+
 
 
 
